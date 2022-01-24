220819827dc424f81185ad9f486bf2516e6395a8
robiul26 <robiulrobi26@gmail.com>
2022-01-25 00:06:57 +0600

    banner complete

---
+++ src/components/Login.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled from 'styled-components';
+const Login = () => {
+    return (
+        <Container>
+            <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt=""/>
+                    <Signup>Get All There</Signup>
+                    <Discription>
+                        Get Premier Access to Raya and the Last Dragon for and additional fee with a Disney+ subscription. As of 01/24/2022, the price of Disney+ and The Disney Bubdle will increase by $1.
+                    </Discription>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt=""/>
+                </CTA>
+                <BgImage/>
+            </Content>
+        </Container>
+    );
+};
+const Container = styled.section`
+overflow:hidden;
+display:flex;
+flex-direction:column;
+text-align:center;
+height:100vh;
+`;
+const Content = styled.div`
+margin-bottom:10vh;
+width: 100%;
+position: relative;
+min-height: 100vh;
+box-sizing: border-box;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 80px 40px;
+height: 100%;
+`;
+const BgImage = styled.div`
+height: 100%;
+background-position: top;
+background-size: cover;
+background-repeat: no-repeat;
+background-image: url('/images/login-background.jpg');
+position: absolute;
+top: 0;
+right: 0;
+left: 0;
+z-index: -1;
+`;
+const CTA = styled.div`
+/* margin-bottom: 2vw; */
+max-width: 650px;
+width: 100%;
+display: flex;
+flex-direction: column;
+`;
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%;
+`;
+const Signup = styled.a`
+font-weight: bold;
+color:#f9f9f9;
+background-color: #0063e5;
+margin-bottom: 12px;
+width: 100%;
+letter-spacing: 1.5px;
+font-size: 18px;
+padding: 16.5px 0;
+border: 1px solid transparent;
+border-radius: 4px;
+text-transform: uppercase;
+cursor: pointer;
+&:hover{
+    background-color: #0483ee;
+}
+`;
+
+const Discription = styled.p`
+color: hsla(0, 0%, 95.3%, 1);
+font-size: 11px;
+margin: 0 0 24px;
+line-height: 1.5;
+letter-spacing: 1.5px;
+`;
+const CTALogoTwo = styled.img`
+margin-bottom: 20px;
+max-width: 600px;
+width: 100%;
+`;
+export default Login;
